6741cb195b6dba7c344a8d582322b7a307edd11b
sharanyakamath <sherrykamath@gmail.com>
2019-01-12 10:00:32 +0530

    add test1

---
+++ test-1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>//Header Files
+#define hundred 100
+int main(){
+    int n,i;//Integer Datatype
+    scanf("%d",&n);//Scan Function
+    char ch;//Character Datatype
+    scanf("%d",&ch);
+    int arr[10];
+    arr[1]=20;
+    for (i=0;i<n;i++){
+        if(i<10){
+            int x;
+            while(x<10){
+                printf("%d\t",x);
+                x++;
+            }
+        }
+
+        else printf("Okay!\n");
+    }
+    /*
+    This File Contains Test cases about Datatypes,Keyword,Identifier,Nested For and while loop,
+    Conditional Statement,Single line Comment,MultiLine Comment etc.*/
+    
+}
