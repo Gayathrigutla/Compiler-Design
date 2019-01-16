f653b2f48ef2174903ca9d4174a5ace0e900bc7b
mehnazyunus <mehnaz138@gmail.com>
2019-01-16 21:51:01 +0530

    Modifications

--- Lexical Analyser/test-1.c
+++ Lexical Analyser/test-1.c
@@ -1,7 +1,7 @@
 #include<stdio.h>//Header Files
 #define hundred 100
 int mainq(int a){
-    int n=10,9i;//Integer Datatype
+    int n=10,9i, !#a;//Integer Datatype
     n=1xabc;
     scanf("%d",&n);//Scan Function
     char ch;//Character Datatype
